f4b5a2eb867c16187ed5a181d4004b2264e41763
Alexey Pleschinger <125436841+AlexeyPleshinger@users.noreply.github.com>
2025-03-17 22:32:54 +0300

    Create LocalizationManager.cs

---
+++ modules/LocalizationManager.cs
@@ -0,0 +1,18 @@
+// LocalizationManager.cs
+private class LocalizationManager
+{
+    private void ChangeLanguage(string newLanguage)
+    {
+        // Логика смены языка
+    }
+
+    private void LoadLanguage(string language)
+    {
+        // Логика загрузки конкретного языкового файла
+    }
+
+    private string GetLocalizedString(string key)
+    {
+        // Логика получения локализованной строки
+    }
+}
