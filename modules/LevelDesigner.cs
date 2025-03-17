9028af98852ba53f878a8473a770010f1b30371f
Alexey Pleschinger <125436841+AlexeyPleshinger@users.noreply.github.com>
2025-03-17 21:28:14 +0300

    Add files via upload

---
+++ modules/LevelDesigner.cs
@@ -0,0 +1,23 @@
+// LevelDesigner.cs
+public class LevelDesigner
+{
+    public void CreateLevel()
+    {
+        // Проектирование уровня
+    }
+
+    public void PopulateEnvironment()
+    {
+        // Наполнение игровых локаций
+    }
+
+    public void HandleTriggers()
+    {
+        // Обработка триггеров
+    }
+
+    public void HandleCutScenes()
+    {
+        // Обработка катсцен
+    }
+}
---
+++ modules/SaveLoadManager.cs
@@ -0,0 +1,13 @@
+// SaveLoadManager.cs
+public class SaveLoadManager
+{
+    public void SaveGame()
+    {
+        // Логика сохранения игры
+    }
+
+    public void LoadGame()
+    {
+        // Логика загрузки игры
+    }
+}
