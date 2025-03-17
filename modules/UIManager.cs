a92e68d68b3a6e48f913a79b294e5ce9374d8416
Alexey Pleschinger <125436841+AlexeyPleshinger@users.noreply.github.com>
2025-03-17 21:27:36 +0300

    Create UIManager.cs

---
+++ modules/UIManager.cs
@@ -0,0 +1,18 @@
+// UIManager.cs
+public class UIManager
+{
+    public void ShowMainMenu()
+    {
+        // Отображение главного меню
+    }
+
+    public void UpdatePlayerStats(int health, int stamina)
+    {
+        // Обновление информации о здоровье и выносливости
+    }
+
+    public void ShowNoteBook()
+    {
+        // Показ записаной книжки игрока
+    }
+}
