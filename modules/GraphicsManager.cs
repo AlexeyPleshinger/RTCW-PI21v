08d05601f6956694a87ffc18b638c14da85b711a
Alexey Pleschinger <125436841+AlexeyPleshinger@users.noreply.github.com>
2025-03-17 22:35:04 +0300

    Create GraphicsManager.cs

---
+++ modules/GraphicsManager.cs
@@ -0,0 +1,23 @@
+// GraphicsManager.cs
+public class GraphicsManager
+{
+    public void Render3DObjects()
+    {
+        // Логика отображения 3D-объектов
+    }
+
+    public void ApplyLighting()
+    {
+        // Реализация освещения
+    }
+
+    public void ApplyShadows()
+    {
+        // Реализация теней
+    }
+
+    public void CreateSpecialEffects()
+    {
+        // Создание эффектов: дым, огонь и т.д.
+    }
+}
